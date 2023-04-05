f5bcc81cf961779d2ca3503a873dfc27f9f398cc
Tran Van Sang <114197833+TranVanSangB2003855@users.noreply.github.com>
2023-04-05 12:00:41 +0700

    Update chatRoom.model.js

--- app/models/chatRoom.model.js
+++ app/models/chatRoom.model.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const chatRoomSchema = new mongoose.Schema({
     message: [{ type: mongoose.Schema.Types.ObjectId, ref: "MESSAGE"}],
     owner: [{ type: mongoose.Schema.Types.ObjectId, ref: "USER"}],
-    createAt: { type: String, require: true },
-    lastMessageDate:  { type: String, require: true }
+    createAt: { type: Date, require: true },
+    lastMessageDate:  { type: Date, require: true }
 })
 
 let CHATROOM = mongoose.model("CHATROOM", chatRoomSchema);
