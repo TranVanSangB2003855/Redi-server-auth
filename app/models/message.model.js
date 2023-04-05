5a43047d81f8548621511badfc7204d66f93bede
Tran Van Sang <114197833+TranVanSangB2003855@users.noreply.github.com>
2023-04-05 12:00:56 +0700

    Update message.model.js

--- app/models/message.model.js
+++ app/models/message.model.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
     content: { type: String, require: true },
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: "USER"},
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "USER"},
     chat: { type: mongoose.Schema.Types.ObjectId, ref: "CHATROOM"},
-    createAt: { type: String, require: true },
+    seen: { type: Boolean, require: true },
+    createAt: { type: Date, require: true },
     type:  { type: String, require: true }
 })
 
