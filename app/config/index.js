5db0a87c0ddff31bb847c8b9552ef0585920e9c8
Tran Van Sang <114197833+TranVanSangB2003855@users.noreply.github.com>
2023-04-05 12:00:15 +0700

    Update index.js

--- app/config/index.js
+++ app/config/index.js
@@ -4,7 +4,7 @@ const config = {
         //process.env.port || 
     },
     db: {
-        uri: "mongodb+srv://admin:TRVASAmb6902@cluster0.ekgkgef.mongodb.net/redi_chat"
+        uri: "mongodb+srv://admin:TRVASAmb6902@cluster0.ekgkgef.mongodb.net/redi"
         //uri: process.env.MONGODB_URI ||"mongodb://127.0.0.1:27017/redi_chat"//"mongodb+srv://admin:TRVASAmb6902@cluster0.ekgkgef.mongodb.net"
         // 
     },
